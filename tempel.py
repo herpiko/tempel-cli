503593a94fd9fe254ce8f5b6785a0ae672b817d0
Herpiko Dwi Aguno <herpiko@gmail.com>
2016-07-11 09:42:15 +0700

    Simplified argv parse

--- tempel.py
+++ tempel.py
@@ -12,35 +12,56 @@ def bantuan():
         -V, --version          output the version number
         -l, --language <lang>  Set language
         -t, --text <text>      From text
-    
+
     tempel.py -l <lang> -t <text>"""
     print string
 
 def main(argv):
+    content = ""
+    lang = "plain"
 
-    try:
-        opts, args = getopt.getopt(argv,"h:l:t:",["language=","text="])
-    except getopt.GetoptError:
+    if len(argv) < 1:
         bantuan()
         sys.exit(2)
-    for opt, arg in opts:
-        if opt == '-h':
+    elif len(argv) == 1:
+        if argv[0] == "-h" or argv[0] == "--help":
             bantuan()
-            sys.exit()
-        elif opt in ("-l", "--language"):
-            lang = arg
-        elif opt in ("-t", "--text"):
-            text = arg
+            sys.exit(2)
+        elif argv[0] == "-t" or argv[0] == "--text" or argv[0] == "-l" or argv[0] == "--language":
+            bantuan()
+            sys.exit(2)
         else:
-            assert False, "unhandled option"
+            f = open(argv[0], "r")
+            content = f.readline()
+    index = 0
+    for arg in argv:
+        index = index + 1
+        if index%2 == 1:
+            if arg == '-l' or arg == '--language':
+                lang = argv[index]
+            elif arg == '-t' or arg == '--text':
+                content = argv[index]
+            else:
+                if index == len(argv):
+                    f = open(arg, "r")
+                    content = f.readline()
+                else:
+                    bantuan()
+                    sys.exit(2)
+
+    if content == "":
+        bantuan()
+        sys.exit(2)
+
     tempel = 'http://tempel.blankon.in'
     values = {
       'language' : lang,
-      'content' : text
+      'content' : content
     }
+    print values
     data = urllib.urlencode(values)
-    print data
-    response = urllib2.urlopen(urllib2.Request(tempel,data)) 
+
+    response = urllib2.urlopen(urllib2.Request(tempel,data))
     print 'URL     :', response.geturl()
 
 if __name__ == "__main__":
